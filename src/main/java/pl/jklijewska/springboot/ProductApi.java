175751b0f4968fbfcb89759589e37315cebaed17
jklijewska <jklijewska@gmail.com>
2020-03-03 09:51:35 +0100

    Learning GetMapping

--- src/main/java/pl/jklijewska/springboot/ProductApi.java
+++ src/main/java/pl/jklijewska/springboot/ProductApi.java
@@ -6,9 +6,28 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/products")
 public class ProductApi {
 
+ /* PARAMETER
     @GetMapping // @RequestMapping(method = RequestMethod.GET)
-    public String getProducts(){
-        return "Hello World with GET";
+    public String getProducts(@RequestParam String name, @RequestParam(required = false, defaultValue = "") String surname){
+        return "Hello "+name+" "+surname;
+    }*/
+
+  /*  PATH
+    @GetMapping("/{name}")
+    public String getProducts(@PathVariable String name){
+        return "Hello "+name;
+    }*/
+
+  //HEADER
+  /*  @GetMapping
+    public String getProducts(@RequestHeader String name){
+        return "Hello "+name;
+    }*/
+
+  //BODY
+    @GetMapping
+    public String getProducts(@RequestBody String name){
+        return "Hello "+name;
     }
 
     @PostMapping
